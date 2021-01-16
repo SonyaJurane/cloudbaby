b5d8064884ba15a1b2a102ba90e00acd2a4512f9
emilyyyzh <emilyy.zhou@mail.utoronto.ca>
2021-01-16 16:11:35 -0500

    made it so that the guy floats near the bottom

--- testing.py
+++ testing.py
@@ -114,7 +114,7 @@ def update(cycle, check, event_number, x):
     if y > max_y:
         y -= action[1]
         action[1] = -action[1]
-    window.geometry('200x80+' + str(x) + '+' + str(y))
+    window.geometry('200x80+' + str(x) + '+' + str(max_y-150))
     label.configure(image=frame)
     window.after(1, event, cycle, check, event_number, x)
 
