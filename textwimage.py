7f3ad00a35bd1e241b2db70f724962494e867915
Sonya Jurane <sonyajurane@gmail.com>
2021-01-16 14:25:15 -0500

    basic version of the speech bubble + image itself

---
+++ textwimage.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+from tkinter import *
+from PIL import Image, ImageTk
+import time
+import os
+
+window = Tk()
+
+#frameCnt = 5
+#frames = [PhotoImage(file='idle.gif', format='gif -index %i' % (i)) for i in range(frameCnt)]
+#text = Label(self, text="Have a nice day!")
+
+image = Image.open('speech_bubble.png')
+tk_image = ImageTk.PhotoImage(image)
+
+#cloudgif = 
+
+speechbubble = tk.Label(window, text='Have a nice day!', image=tk_image, compound='center', bd=0)
+speechbubble.config(font=("Courier 10 bold"))
+
+window.overrideredirect(True) #remove the border
+
+window.wm_attributes('-transparentcolor', 'black')
+
+speechbubble.pack()
+
+window.mainloop()
